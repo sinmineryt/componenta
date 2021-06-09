5603bd3af09aad6e3b1d1780104e3dcef8dc39bb
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-06-09 16:14:40 +0200

    Initial documentation

---
+++ docs/js/components.js
@@ -0,0 +1,14 @@
+class Navbar extends Component {
+  constructor() {
+    super("navbar");
+  }
+
+  html() {
+    return `
+    <h3>Componenta</h3>
+    <div class="nav-items">
+      <a href="index.html">Home</a>
+    </div>
+    `;
+  }
+}
